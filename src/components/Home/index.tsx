0267100add6deee99e49cfea79a274577491098e
David Gudeman <dcgudeman@gmail.com>
2021-06-27 19:01:40 -0700

    fixed cloud picture styling

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -90,7 +90,7 @@ const Home: React.FC = () => {
                     efficient, fast, and truly decentralized.
                   </span>
                 </h1>
-                <div className="mt-10 sm:mt-12">
+                <div className="mt-10 pb-6 sm:mt-12">
                   <p className="mt-3 mb-2 w-full sm:w-1/2 mx-auto lg:mx-0 text-sm text-gray-300 sm:mt-4">
                     {"Don't miss the latest news."}
                   </p>
@@ -104,7 +104,7 @@ const Home: React.FC = () => {
                 </div>
               </div>
             </div>
-            <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
+            <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative hidden md:block">
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
                 <img
                   className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
