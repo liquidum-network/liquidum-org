c9e007b20943fce8e471c1b893259b5aefc49e24
Michael Graczyk <mgraczyk@google.com>
2021-06-27 17:10:29 -0700

    add testnet CTA and change text

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import {
-  CloudUploadIcon,
+  UserGroup,
   RefreshIcon,
   GlobeIcon,
 } from '@heroicons/react/outline';
 
 const features = [
   {
-    name: 'Ethereum Ecosystem Compatible',
-    description: 'MetaMask, Etherscan, Solidity and other common Ethereum compatible ecosystem components will work out of the box.',
-    icon: CloudUploadIcon,
+    name: 'Ethereum Ecosystem',
+    description: 'MetaMask, Solidity, Etherscan, EVM, and more. A massive ecosystem of tools and infrastructure supported out of the box.',
+    icon: UserGroup,
   },
   {
     name: 'Sustainable',
-    description: 'Leveraging a Proof-of-Space consensus engine farmers/miners will use somewhere between 300 and 10,000 times less electricity for the same result as Proof-of-Work.',
+    description: "Uses Chia's Proof of Space and Time to power consensus without the waste of Proof of Work or centralization and complexity of Proof of Stake.",
     icon: GlobeIcon,
   },
   {
     name: 'Decentralized',
-    description: 'To participate in the network all that is required is commonly available commodity hardware.',
+    description: 'Anyone with 100GB of free hard drive space can mine and earn rewards for securing the network. Compatible with existing Chia plots.',
     icon: RefreshIcon,
   },
 ];
@@ -85,30 +85,27 @@ const Home: React.FC = () => {
                   <span className="block">Liquidum</span>
                 </h1>
                 <h1 className="mt-5 text-xl tracking-tight font-bold text-white sm:mt-3 sm:text-3xl lg:mt-3 xl:text-3xl">
-                  <span className="block">Finally, an EVM ecosystem with a sustainable,</span>
+                  <span className="block">A new Ethereum Smart Contract blockchain that is</span>
                   <span className="pb-3 block bg-clip-text text-transparent bg-gradient-to-r from-teal-200 to-cyan-400 sm:pb-5">
-                    truly decentralized consensus engine.
+                    efficient, fast, and truly decentralized.
                   </span>
                 </h1>
                 <div className="mt-10 sm:mt-12">
                   <p className="mt-3 mb-2 w-full sm:w-1/2 mx-auto lg:mx-0 text-sm text-gray-300 sm:mt-4">
-                    Sign up for the latest news.
+                    Don't miss the latest news.
                   </p>
                   <button
                     type="submit"
                     className="block w-full sm:w-1/2 mx-auto lg:mx-0 py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white  font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
                     onClick={() => { location.href = 'https://b69f94bd.sibforms.com/serve/MUIEADAUc0WBVDOwB2AKAHJyDb3L5HdyLhxy0434l8EK6wcd3TU8SXHKO6hcZatvm2dAI685GG7o3uCn3O3eBRjPi3bPhKbkdo9aRXxmyrfdUSAr8nNNa-9XIy-rUTH8QxWRyYovwEUuOJrneBfM7ysk9clVsbOCiLdpSvygqrYujEXmGWWXR_wYs6x9YTpLsE9OjAxOEJTa5anq'; }}
                   >
-                    Sign Up
+                    Subscribe
                   </button>
                 </div>
               </div>
             </div>
             <div className="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0">
-                <p className="text-base text-gray-300 sm:text-xl lg:text-lg xl:text-xl">
-                  A new blockchain and smart transaction platform that is easier to use, more efficient, and secure.
-                </p>
                 <img
                   className="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                   src="/cloud-illustration-teal-cyan.svg"
@@ -120,38 +117,11 @@ const Home: React.FC = () => {
         </div>
       </div>
 
-      {/* Feature section with screenshot */}
-      {/* <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
-        <div className="mx-auto max-w-md px-4 text-center sm:px-6 sm:max-w-3xl lg:px-8 lg:max-w-7xl">
-          <div>
-            <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Serverless</h2>
-            <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-              No server? No problem.
-            </p>
-            <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-              Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis nunc, ullamcorper
-              malesuada. Eleifend condimentum id viverra nulla.
-            </p>
-          </div>
-          <div className="mt-12 -mb-10 sm:-mb-24 lg:-mb-80">
-            <img
-              className="rounded-lg shadow-xl ring-1 ring-black ring-opacity-5"
-              src="https://tailwindui.com/img/component-images/green-project-app-screenshot.jpg"
-              alt=""
-            />
-          </div>
-        </div>
-      </div> */}
-
       {/* Feature section with grid */}
       <div className="relative bg-white py-16 sm:py-24 lg:py-32">
         <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl">
-          <h2 className="text-base font-semibold tracking-wider text-cyan-600 uppercase">Sustainable Ethereum</h2>
           <p className="mt-2 text-3xl font-extrabold text-gray-900 tracking-tight sm:text-4xl">
-            Everything you love about Ethereum minus the carbon footprint
-          </p>
-          <p className="mt-5 max-w-prose mx-auto text-xl text-gray-500">
-            Be a part of the decentralized, green future
+            The Ethereum EVM Ecosystem powered by Chia's Proof of Space and Time
           </p>
           <div className="mt-12">
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
@@ -275,13 +245,8 @@ const Home: React.FC = () => {
       </div> */}
 
       {/* CTA Section */}
-      {/* <div className="relative bg-gray-900">
+      <div className="relative bg-gray-900">
         <div className="relative h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-          <img
-            className="w-full h-full object-cover"
-            src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&sat=-100"
-            alt=""
-          />
           <div
             aria-hidden="true"
             className="absolute inset-0 bg-gradient-to-r from-teal-500 to-cyan-600 mix-blend-multiply"
@@ -290,28 +255,14 @@ const Home: React.FC = () => {
         <div className="relative mx-auto max-w-md px-4 py-12 sm:max-w-7xl sm:px-6 sm:py-20 md:py-28 lg:px-8 lg:py-32">
           <div className="md:ml-auto md:w-1/2 md:pl-10">
             <h2 className="text-base font-semibold uppercase tracking-wider text-gray-300">
-              Award winning support
+              Testnet launching 08/11/2021
             </h2>
-            <p className="mt-2 text-white text-3xl font-extrabold tracking-tight sm:text-4xl">We’re here to help</p>
-            <p className="mt-3 text-lg text-gray-300">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
-              scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum
-              tincidunt duis.
-            </p>
-            <div className="mt-8">
-              <div className="inline-flex rounded-md shadow">
-                <a
-                  href="#"
-                  className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50"
-                >
-                  Visit the help center
-                  <ExternalLinkIcon className="-mr-1 ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
-                </a>
-              </div>
-            </div>
+            <h2 className="text-base font-semibold uppercase tracking-wider text-gray-300">
+              Mainnet Q4 2021
+            </h2>
           </div>
         </div>
-      </div> */}
+      </div>
     </React.Fragment>
   );
 };
