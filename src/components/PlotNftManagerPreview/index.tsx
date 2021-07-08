0b90d522cd7d699da53d01a86cc13359ffcf2d4c
David Gudeman <dcgudeman@gmail.com>
2021-07-07 21:32:40 -0700

    updated new file to use spaces instead of tabs

--- src/components/PlotNftManagerPreview/index.tsx
+++ src/components/PlotNftManagerPreview/index.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
 const PlotNftManagerPreview: React.FC = () => {
-	return (
-		<div className="bg-white max-w-7xl mx-auto mt-3">
-			<div className="mx-3 mt-4 text-2xl font-semibold">Preview of our plotnft manager Dapp.</div>
-			<div className="bg-gray-50 shadow border rounded-lg mt-6">
-				<div className="px-4 py-5 sm:px-6 text-xl">
-					You can view details, create, and update your plotnfts. Easily vew attached pools and total rewards collected.
-				</div>
-				<div className="bg-gray-100 px-4 py-5 sm:p-6">
-					<img className="border p-1 shadow" src="https://firebasestorage.googleapis.com/v0/b/proof-of-sprout.appspot.com/o/static-assets%2Fplot-nft-manager-1.png?alt=media" alt="plotnft manager 1" />
-				</div>
-			</div>
-			<div className="bg-gray-50 shadow border rounded-lg mt-6 mb-3">
-				<div className="px-4 py-5 sm:px-6 text-xl">
-					{"It's easy to switch pools."} You can search and compare details like pay structure before switching.
-				</div>
-				<div className="bg-gray-100 px-4 py-5 sm:p-6">
-					<img className="border p-1 shadow" src="https://firebasestorage.googleapis.com/v0/b/proof-of-sprout.appspot.com/o/static-assets%2Fplot-nft-manager-2.png?alt=media" alt="plotnft manager 2" />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-white max-w-7xl mx-auto mt-3">
+      <div className="mx-3 mt-4 text-2xl font-semibold">Preview of our plotnft manager Dapp.</div>
+      <div className="bg-gray-50 shadow border rounded-lg mt-6">
+        <div className="px-4 py-5 sm:px-6 text-xl">
+          You can view details, create, and update your plotnfts. Easily vew attached pools and total rewards collected.
+        </div>
+        <div className="bg-gray-100 px-4 py-5 sm:p-6">
+          <img className="border p-1 shadow" src="https://firebasestorage.googleapis.com/v0/b/proof-of-sprout.appspot.com/o/static-assets%2Fplot-nft-manager-1.png?alt=media" alt="plotnft manager 1" />
+        </div>
+      </div>
+      <div className="bg-gray-50 shadow border rounded-lg mt-6 mb-3">
+        <div className="px-4 py-5 sm:px-6 text-xl">
+          {"It's easy to switch pools."} You can search and compare details like pay structure before switching.
+        </div>
+        <div className="bg-gray-100 px-4 py-5 sm:p-6">
+          <img className="border p-1 shadow" src="https://firebasestorage.googleapis.com/v0/b/proof-of-sprout.appspot.com/o/static-assets%2Fplot-nft-manager-2.png?alt=media" alt="plotnft manager 2" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PlotNftManagerPreview;
